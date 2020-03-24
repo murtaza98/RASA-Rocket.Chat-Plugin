cc0590e9c1041d676412f0061022e0321564cf09
murtaza98 <m.patrawala@somaiya.edu>
2020-03-24 20:45:14 +0530

    updated handover feature
    More Info
    - added feature to provide target department in app setting
    - added new app setting, Bot Password(Required for REST API authentication)

--- RasaPluginApp.ts
+++ RasaPluginApp.ts
@@ -32,6 +32,9 @@ export class RasaPluginApp extends App implements IPostMessageSent {
         message: IMessage, read: IRead, http: IHttp, persistence: IPersistence,
         modify: IModify): Promise<void> {
         const SettingBotUsername: string = (await read.getEnvironmentReader().getSettings().getById('Lc-Bot-Username')).value;
+        const SettingBotPassword: string = (await read.getEnvironmentReader().getSettings().getById('Lc-Bot-Password')).value;
+        const targetDeptName: string = (await read.getEnvironmentReader().getSettings().getById('Handover-Target-Department-Name')).value;
+
         if (message.sender.username === SettingBotUsername) {
             return;
         } else if (message.room.type !== 'l') {
@@ -47,9 +50,9 @@ export class RasaPluginApp extends App implements IPostMessageSent {
 
         // Debug
         // this.getLogger().log(`app.${ this.getNameSlug() }`);
-        this.getLogger().log(message.sender.username);
-        this.getLogger().log('Room type--> ' + message.room.type);
-        this.getLogger().log('roles--> ' + message.sender.roles);
+        this.getLogger().log('message sent by username --> ' + message.sender.username);
+        this.getLogger().log('Room type --> ' + message.room.type);
+        this.getLogger().log('roles --> ' + message.sender.roles);
         this.getLogger().log('served by --> ' + lBotUser.username);
 
         if (SettingBotUsername !== lBotUser.username) {
@@ -59,14 +62,14 @@ export class RasaPluginApp extends App implements IPostMessageSent {
         // CHECK HANDOVER
         if (message.text === 'perform_handover') {
             this.getLogger().log('handover action start');
-            // perform authentication
+            // http_request_no_1 ---> perform authentication
             const authHttpRequest: IHttpRequest = {
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 data: {
-                    user: 'murtaza98',
-                    password: '12345',
+                    user: SettingBotUsername,
+                    password: SettingBotPassword,
                 },
             };
 
@@ -74,44 +77,55 @@ export class RasaPluginApp extends App implements IPostMessageSent {
                 (loginResponse) => {
                     // console.log(loginResponse.content);
                     const loginResponseJSON = JSON.parse((loginResponse.content || '{}'));
-                    console.log(loginResponseJSON.data.userId);
-                    console.log(loginResponseJSON.data.authToken);
-                    console.log(message.room.id);
+                    console.log('loginResponseJSON.data.userId --> ' + loginResponseJSON.data.userId);
+                    console.log('loginResponseJSON.data.authToken --> ' + loginResponseJSON.data.authToken);
+                    console.log('message.room.id --> ' + message.room.id);
 
-                    const ForwardHttpRequest: IHttpRequest = {
+                    const deptHttpRequest: IHttpRequest = {
                         headers: {
-                            'Content-Type': 'application/json',
                             'X-Auth-Token': loginResponseJSON.data.authToken,
                             'X-User-Id': loginResponseJSON.data.userId,
                         },
-                        data: {
-                            roomId: message.room.id,
-                            departmentId: 'FaNCjpwyqYD49LZdK',
-                        },
                     };
+                    // http_request_no_2 --> get department id
+                    http.get('http://localhost:3000/api/v1/livechat/department', deptHttpRequest).then(
+                        (deptResponse) => {
+                            const deptResponseJSON = JSON.parse((deptResponse.content || '{}'));
+                            // console.log(deptResponseJSON);
+                            let targetDeptId: string = '';
+                            deptResponseJSON.departments.forEach(
+                                (department) => {
+                                    if (department.name === targetDeptName) {
+                                        targetDeptId = department._id;
+                                    }
+                                },
+                            );
 
-                    http.post('http://localhost:3000/api/v1/livechat/room.forward', ForwardHttpRequest).then(
-                        (forwardResponse) => {
-                            console.log(forwardResponse);
+                            console.log('Target Dept Id --> ' + targetDeptId);
+
+                            // http_request_no_3 --> make handover request
+                            const ForwardHttpRequest: IHttpRequest = {
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    'X-Auth-Token': loginResponseJSON.data.authToken,
+                                    'X-User-Id': loginResponseJSON.data.userId,
+                                },
+                                data: {
+                                    roomId: message.room.id,
+                                    departmentId: targetDeptId,
+                                },
+                            };
+                            http.post('http://localhost:3000/api/v1/livechat/room.forward', ForwardHttpRequest).then(
+                                (forwardResponse) => {
+                                    console.log('room.forward response --> ' + forwardResponse);
+                                },
+                            );
                         },
                     );
                 },
             );
-
-            // parse access_token
-
-
-            // request room.forward endpoint
-
-
-
-
             return;
         }
-
-
-
-
 
         // --> content of post message
         // {
@@ -183,7 +197,25 @@ export class RasaPluginApp extends App implements IPostMessageSent {
             i18nLabel: 'Livechat Bot Username',
             required: true,
         };
+        const LcBotPassword: ISetting = {
+            id: 'Lc-Bot-Password',
+            public: true,
+            type: SettingType.STRING,
+            packageValue: '',
+            i18nLabel: 'Livechat Bot Password',
+            required: true,
+        };
+        const HandoverTargetDepartmentName: ISetting = {
+            id: 'Handover-Target-Department-Name',
+            public: true,
+            type: SettingType.STRING,
+            packageValue: '',
+            i18nLabel: 'Handover Target Department Name',
+            required: true,
+        };
         configuration.settings.provideSetting(RasaServerISetting);
         configuration.settings.provideSetting(LcBotUsername);
+        configuration.settings.provideSetting(LcBotPassword);
+        configuration.settings.provideSetting(HandoverTargetDepartmentName);
     }
 }
